32eef3a6d8fd263eeea4baf4e0356fcc32f15cf2
Daniel Kaspo <nessdan@users.noreply.github.com>
2019-10-08 23:27:26 -0400

    Defer script loading, separate JS out.

---
+++ firebase-init.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCLTOrsPbBrCXu-WTEKDYO5c9lglog9lcU",
+  authDomain: "keyboard-controller-waitlist.firebaseapp.com",
+  databaseURL: "https://keyboard-controller-waitlist.firebaseio.com",
+  projectId: "keyboard-controller-waitlist",
+  storageBucket: "keyboard-controller-waitlist.appspot.com",
+  messagingSenderId: "757579060743",
+  appId: "1:757579060743:web:64a5b6b35c55e2fec13ca2",
+  measurementId: "G-TRS5XJHX3B"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
